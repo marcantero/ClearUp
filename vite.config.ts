b7eee846d08f823b7e812789334e7e7500dc7835
Marc Cantero <148981890+NIU1710710@users.noreply.github.com>
2026-09-14 20:34:34 +0200

    Fix base path to /ClearUp/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ClearCut/',
+  base: '/ClearUp/',
   plugins: [react()],
   optimizeDeps: {
     // Transformers.js incluye código avanzado que no se lleva bien
